c3439705b3443882fc3da304922e4301aea2bd62
Aayush saini <ayushsaini2308@gmail.com>
2025-06-29 15:09:17 +0530

    minor change

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/', schoolRoutes);
 
+app.get("/", (req, res) => {
+  res.send("School API is live!");
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
